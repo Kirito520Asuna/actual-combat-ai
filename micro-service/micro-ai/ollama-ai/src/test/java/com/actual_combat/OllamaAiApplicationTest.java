4124f0c71725a2451c8de7b528e035b47061a45b
yan <doutianmianxia@qq.com>
2025-05-16 18:01:46 +0800

    feat(ollama-ai): 增加Flux流处理和日志配置

--- micro-service/micro-ai/ollama-ai/src/test/java/com/actual_combat/OllamaAiApplicationTest.java
+++ micro-service/micro-ai/ollama-ai/src/test/java/com/actual_combat/OllamaAiApplicationTest.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 
 /**
  * @Author yan
@@ -19,5 +20,12 @@ public class OllamaAiApplicationTest {
         ChatClient chatClient = SpringUtil.getBean(ChatClient.class);
         String content = chatClient.prompt().user("你是谁？").call().content();
         log.info("content:{}",content);
+        //Flux<String> content = chatClient.prompt().user("你是谁？").stream().content();
+        //// 订阅 Flux 用于调试日志
+        //content.doOnNext(chunk -> log.warn("Stream chunk: {}", chunk))
+        //        .doOnError(error -> log.error("Stream error: {}", error.getMessage()))
+        //        .doOnComplete(() -> log.warn("Stream completed"))
+        //        .subscribe(); // 确保订阅以触发流
+
     }
 }

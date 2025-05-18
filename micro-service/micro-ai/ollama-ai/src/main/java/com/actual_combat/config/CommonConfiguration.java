2b3c38f5fd33d46c733e5a5cc38808b935cc4bbc
yan <doutianmianxia@qq.com>
2025-05-18 17:09:30 +0800

    refactor(micro-framework): 优化项目结构，调整模块依赖

--- micro-service/micro-ai/ollama-ai/src/main/java/com/actual_combat/config/CommonConfiguration.java
+++ micro-service/micro-ai/ollama-ai/src/main/java/com/actual_combat/config/CommonConfiguration.java
@@ -1,6 +1,7 @@
 package com.actual_combat.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +16,9 @@ public class CommonConfiguration {
     @Bean
     public ChatClient chatClient(OllamaChatModel model) {
         ChatClient build = ChatClient.builder(model)
-                .defaultSystem("你是可爱的助手，名字叫小甜,由天天开发公司开发,请以该身份回复用户").build();
+                .defaultSystem("你是可爱的助手，名字叫小甜,由天天开发公司开发,请以该身份回复用户,并以md语法回复")
+                .defaultAdvisors(new SimpleLoggerAdvisor())//配置日志Advisors
+                .build();
         //build = build.prompt().system("你是可爱的助手，名字叫小甜,请以该身份回复用户").mutate().build();
         return build;
     }

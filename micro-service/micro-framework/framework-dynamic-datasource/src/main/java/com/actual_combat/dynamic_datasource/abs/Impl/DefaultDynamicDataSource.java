393aa7f429fbd5eceb018586c13d5ca3c6066343
yan <doutianmianxia@qq.com>
2025-05-18 16:26:49 +0800

    feat: 添加动态数据源模块及配置

---
+++ micro-service/micro-framework/framework-dynamic-datasource/src/main/java/com/actual_combat/dynamic_datasource/abs/Impl/DefaultDynamicDataSource.java
@@ -0,0 +1,17 @@
+package com.actual_combat.dynamic_datasource.abs.Impl;
+
+import com.actual_combat.dynamic_datasource.abs.AbsDynamicDataSource;
+import com.actual_combat.dynamic_datasource.config.DynamicDataSourceConfig;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author yan
+ * @Date 2025/5/18 16:20:37
+ * @Description
+ */
+
+@Service
+@ConditionalOnBean(DynamicDataSourceConfig.class)
+public class DefaultDynamicDataSource implements AbsDynamicDataSource {
+}
---
+++ micro-service/micro-framework/framework-dynamic-datasource/src/main/java/com/actual_combat/dynamic_datasource/config/DynamicDataSourceConfig.java
@@ -0,0 +1,42 @@
+package com.actual_combat.dynamic_datasource.config;
+
+import cn.hutool.extra.spring.SpringUtil;
+import com.actual_combat.dynamic_datasource.abs.AbsDynamicDataSource;
+import com.baomidou.dynamic.datasource.provider.DynamicDataSourceProvider;
+import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceAutoConfiguration;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * @Author yan
+ * @Date 2025/5/18 16:11:33
+ * @Description
+ */
+
+@Configuration
+@AutoConfigureBefore({DynamicDataSourceAutoConfiguration.class, SpringBootConfiguration.class})
+public class DynamicDataSourceConfig {
+    private AbsDynamicDataSource absDynamicDataSource() {
+        return SpringUtil.getBean(AbsDynamicDataSource.class);
+    }
+
+    @Bean
+    public DataSource initDataSource() throws Exception {
+        return absDynamicDataSource().dataSource();
+    }
+
+    /**
+     * 将shardingDataSource放到了多数据源（dataSourceMap）中
+     * 注意有个版本的bug，3.1.1版本 不会进入loadDataSources 方法，这样就一直造成数据源注册失败
+     */
+
+    @Bean
+    public DynamicDataSourceProvider initDynamicDataSourceProvider() throws Exception {
+        return absDynamicDataSource().dynamicDataSourceProvider();
+    }
+}

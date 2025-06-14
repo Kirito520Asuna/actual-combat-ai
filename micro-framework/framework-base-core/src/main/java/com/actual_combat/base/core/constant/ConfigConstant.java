5a28033aeb1b22cbe8cf105f30811417a445590f
yan <doutianmianxia@qq.com>
2025-06-14 14:01:30 +0800

    feat(ConfigConstant): 增加配置常量及路径定义

--- micro-framework/framework-base-core/src/main/java/com/actual_combat/base/core/constant/ConfigConstant.java
+++ micro-framework/framework-base-core/src/main/java/com/actual_combat/base/core/constant/ConfigConstant.java
@@ -6,9 +6,29 @@ package com.actual_combat.base.core.constant;
  * @Description
  */
 public interface ConfigConstant {
-    String AUTH_CONFIG = "config.auth";
-    String AUTH_SECURITY_CONFIG = AUTH_CONFIG + ".security";
-    String AUTH_SHIRO_CONFIG = AUTH_CONFIG + ".shiro";
-    String AUTH_SECURITY_ANNOTATION_CONFIG = AUTH_SECURITY_CONFIG + ".annotation";
-    String AUTH_SHIRO_ANNOTATION_CONFIG = AUTH_SHIRO_CONFIG + ".annotation";
+    String CONFIG = "config";
+    String AUTH = "auth";
+    String SECURITY = "security";
+    String SHIRO = "shiro";
+    String ANNOTATION = "annotation";
+    String NAME = "name";
+
+    String FULL_PATH="full-path";
+
+    String DOMAINS = "domains";
+    String CORS= "cors";
+    String GATEWAY= "gateway";
+
+    String AUTH_CONFIG = CONFIG + "." + AUTH;
+    String AUTH_SECURITY_CONFIG = AUTH_CONFIG + "." + SECURITY;
+    String AUTH_SHIRO_CONFIG = AUTH_CONFIG + "." + SHIRO;
+    String AUTH_SECURITY_ANNOTATION_CONFIG = AUTH_SECURITY_CONFIG + "." + ANNOTATION;
+    String AUTH_SHIRO_ANNOTATION_CONFIG = AUTH_SHIRO_CONFIG + "." + ANNOTATION;
+
+    String GATEWAY_CONFIG = CONFIG + "." + GATEWAY;
+    String GATEWAY_DOMAINS_CONFIG = GATEWAY_CONFIG + "." + DOMAINS;
+    String GATEWAY_DOMAINS_NAME_CONFIG = GATEWAY_DOMAINS_CONFIG + "." + NAME;
+    String GATEWAY_DOMAINS_FULL_PATH_CONFIG = GATEWAY_DOMAINS_CONFIG + "." + FULL_PATH;
+
+    String GATEWAY_CORS_CONFIG = GATEWAY_CONFIG + "." + CORS;
 }

1197072c80b9c230e02a780727833f0bef2b3b75
yan <doutianmianxia@qq.com>
2025-06-17 23:28:03 +0800

    feat: 添加EasyElasticsearch模块及配置

---
+++ micro-framework/framework-easy-elasticsearch/src/main/java/com/actual_combat/easy/elasticsearch/service/impl/SimpleEasyElasticsearchService.java
@@ -0,0 +1,11 @@
+package com.actual_combat.easy.elasticsearch.service.impl;
+
+import com.actual_combat.easy.elasticsearch.service.EasyElasticsearchService;
+
+/**
+ * @Author yan
+ * @Date 2024/8/13 0013 16:12:17
+ * @Description
+ */
+public class SimpleEasyElasticsearchService implements EasyElasticsearchService {
+}

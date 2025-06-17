f293a090f2a59b82b104971b6f632635c8fb1ad6
yan <doutianmianxia@qq.com>
2025-06-17 11:56:10 +0800

    fix: 修复AbsAuthorization接口代码格式及逻辑错误

--- micro-framework/framework-basic-core/src/main/java/com/actual_combat/basic/core/abs/auth/core/AbsAuthorization.java
+++ micro-framework/framework-basic-core/src/main/java/com/actual_combat/basic/core/abs/auth/core/AbsAuthorization.java
@@ -28,7 +28,7 @@ import java.util.Map;
 public interface AbsAuthorization extends AbsBean {
     @Override
     default void init() {
-        log().debug("[Bean]-[Authorization]-[init]::[{}]: ",getAClassName());
+        log().debug("[Bean]-[Authorization]-[init]::[{}]: ", getAClassName());
     }
 
     /**
@@ -47,30 +47,32 @@ public interface AbsAuthorization extends AbsBean {
 
     /**
      * 重新生成双token 推送至响应器
+     *
      * @param userId
      * @param tokenName
      * @param refreshTokenName
      * @param response
      */
-    default Map<String, String>  pushTwoToken(String userId, String tokenName, String refreshTokenName, HttpServletResponse response) {
+    default Map<String, String> pushTwoToken(String userId, String tokenName, String refreshTokenName, HttpServletResponse response) {
         String token = JwtUtils.createJWT(userId);
         Map<String, String> hashMap = Maps.newLinkedHashMap();
         hashMap.put(tokenName, token);
         String msg = "one-";
-        if (StrUtil.isNotBlank(refreshTokenName)){
+        if (StrUtil.isNotBlank(refreshTokenName)) {
             String refreshToken = JwtUtils.createJWT(userId, JwtUtils.getLONG_JWT_TTL());
             hashMap.put(refreshTokenName, refreshToken);
             msg = "two-";
         }
-        ResponseUtils.responseSetHeader(response,hashMap);
-        log().debug("[{}]生成{}token推送至响应器",getAClass().getName(),msg);
+        ResponseUtils.responseSetHeader(response, hashMap);
+        log().debug("[{}]生成{}token推送至响应器", getAClass().getName(), msg);
         return hashMap;
     }
 
     /**
      * 获取用户ID 通过refreshToken 并重新生成双token 推送至响应器
-     * @param secret 秘钥
-     * @param tokenName token名称
+     *
+     * @param secret           秘钥
+     * @param tokenName        token名称
      * @param refreshTokenName refreshToken名称
      * @param request
      * @param response
@@ -99,22 +101,23 @@ public interface AbsAuthorization extends AbsBean {
             e.printStackTrace();
             throw new GlobalCustomException(ApiCode.UNAUTHORIZED);
         }
-        log().debug("[{}]刷新长token处理",getAClass().getName());
+        log().debug("[{}]刷新长token处理", getAClass().getName());
         return userId;
     }
 
     /**
      * 获取用户ID 通过Token
-     * @param openTwoToken 是否开启双token？
-     * @param secret 秘钥
-     * @param tokenName token名称
+     *
+     * @param openTwoToken     是否开启双token？
+     * @param secret           秘钥
+     * @param tokenName        token名称
      * @param refreshTokenName refreshToken名称
      * @param request
      * @param response
      * @return
      */
     default String getUserIdByToken(boolean openTwoToken, String secret, String tokenName, String refreshTokenName, HttpServletRequest request, HttpServletResponse response) {
-        tokenName = ObjectUtils.defaultIfEmpty(tokenName,JwtUtils.HEADER_AS_TOKEN);
+        tokenName = ObjectUtils.defaultIfEmpty(tokenName, JwtUtils.HEADER_AS_TOKEN);
         //获取token
         String token = request.getHeader(tokenName);
         token = getTokenBySubstring(token);
@@ -136,16 +139,22 @@ public interface AbsAuthorization extends AbsBean {
             } else {
                 userId = claims.getSubject();
                 String msg = "two-token模式";
-                if (!openTwoToken){
+                if (!openTwoToken) {
                     msg = "one-token模式";
-                    Long refreshOneTokenLong = ObjectUtils.defaultIfEmpty(SpringUtil.getBean(JwtConfig.class).getRefreshOneTokenLong(),5l);
+                    JwtConfig jwtConfig = null;
+                    try {
+                        jwtConfig = SpringUtil.getBean(JwtConfig.class);
+                    } catch (Exception e) {
+                        log().warn("[{}]获取jwtConfig失败,error:{}", getAClass().getName(), e.getMessage());
+                    }
+                    Long refreshOneTokenLong = ObjectUtils.defaultIfEmpty(jwtConfig == null ? null : jwtConfig.getRefreshOneTokenLong(), 5l);
                     Date date = DateUtils.LocalDateTimeToDate(LocalDateTime.now().plusMinutes(refreshOneTokenLong));
                     boolean tokenExpiredPass = JwtUtils.isTokenExpired(claims, date);
-                    if (tokenExpiredPass){
+                    if (tokenExpiredPass) {
                         pushTwoToken(userId, tokenName, null, response);
                     }
                 }
-                log().debug("[{}][{}]获取用户ID通过Token",getAClass().getName(),msg);
+                log().debug("[{}][{}]获取用户ID通过Token", getAClass().getName(), msg);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -156,11 +165,12 @@ public interface AbsAuthorization extends AbsBean {
 
     /**
      * 检查token是否合法
+     *
      * @param request
      * @param response
      * @return
      */
-    default boolean checkToken(HttpServletRequest request, HttpServletResponse response){
+    default boolean checkToken(HttpServletRequest request, HttpServletResponse response) {
         return true;
     }
 }

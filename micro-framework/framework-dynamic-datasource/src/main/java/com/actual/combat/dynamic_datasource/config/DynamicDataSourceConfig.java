02f0a6b1c2754086349f1b6dfe3469b6e856a3f7
yan <doutianmianxia@qq.com>
2025-06-19 03:23:22 +0800

    build: 升级多数据源及依赖版本至4.0.0

--- micro-framework/framework-dynamic-datasource/src/main/java/com/actual/combat/dynamic_datasource/config/DynamicDataSourceConfig.java
+++ micro-framework/framework-dynamic-datasource/src/main/java/com/actual/combat/dynamic_datasource/config/DynamicDataSourceConfig.java
@@ -2,12 +2,18 @@ package com.actual.combat.dynamic_datasource.config;
 
 import cn.hutool.extra.spring.SpringUtil;
 import com.actual.combat.dynamic_datasource.abs.AbsDynamicDataSource;
+import com.actual.combat.dynamic_datasource.abs.Impl.DefaultDynamicDataSource;
+import com.baomidou.dynamic.datasource.creator.DefaultDataSourceCreator;
 import com.baomidou.dynamic.datasource.provider.DynamicDataSourceProvider;
+import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceAssistConfiguration;
 import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceAutoConfiguration;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
@@ -20,11 +26,17 @@ import javax.sql.DataSource;
 @Configuration
 @AutoConfigureBefore({DynamicDataSourceAutoConfiguration.class, SpringBootConfiguration.class})
 public class DynamicDataSourceConfig {
+    @Bean
+    @ConditionalOnMissingBean(AbsDynamicDataSource.class)
+    public AbsDynamicDataSource dynamicDataSource() {
+        return new DefaultDynamicDataSource();
+    }
     private AbsDynamicDataSource absDynamicDataSource() {
         return SpringUtil.getBean(AbsDynamicDataSource.class);
     }
 
     @Bean
+    @ConditionalOnMissingBean(DataSource.class)
     public DataSource initDataSource() throws Exception {
         return absDynamicDataSource().dataSource();
     }
@@ -34,8 +46,9 @@ public class DynamicDataSourceConfig {
      * 注意有个版本的bug，3.1.1版本 不会进入loadDataSources 方法，这样就一直造成数据源注册失败
      */
 
-    @Bean
-    public DynamicDataSourceProvider initDynamicDataSourceProvider() throws Exception {
-        return absDynamicDataSource().dynamicDataSourceProvider();
+    @ConditionalOnMissingBean({DynamicDataSourceAssistConfiguration.class,DynamicDataSourceProvider.class})
+    //@Bean @Primary
+    public DynamicDataSourceProvider initDynamicDataSourceProvider(DefaultDataSourceCreator defaultDataSourceCreator) throws Exception {
+        return absDynamicDataSource().dynamicDataSourceProvider(defaultDataSourceCreator);
     }
 }

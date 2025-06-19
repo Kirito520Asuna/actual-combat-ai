8b11aebe6a090cab590a27ce4eb1f9fe8d529ca0
yan <doutianmianxia@qq.com>
2025-06-19 09:26:15 +0800

    feat: 扩展抽象接口继承AbsBean

--- micro-framework/framework-basic-core/src/main/java/com/actual/combat/basic/core/abs/bean/AbstractShiroBean.java
+++ micro-framework/framework-basic-core/src/main/java/com/actual/combat/basic/core/abs/bean/AbstractShiroBean.java
@@ -1,9 +1,11 @@
 package com.actual.combat.basic.core.abs.bean;
 
+import com.actual.combat.aop.abs.bean.AbsBean;
+
 /**
  * @Author yan
  * @Date 2025/6/14 01:55:33
  * @Description
  */
-public interface AbstractShiroBean {
+public interface AbstractShiroBean extends AbsBean {
 }

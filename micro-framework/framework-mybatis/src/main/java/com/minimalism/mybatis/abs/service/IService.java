30303eac92efd6b475c2207e02f2e2320b2870c6
yan <doutianmianxia@qq.com>
2025-06-30 22:27:41 +0800

    refactor: 重命名并重构MyBatis服务与映射接口

--- micro-framework/framework-mybatis/src/main/java/com/minimalism/mybatis/abs/service/IService.java
+++ micro-framework/framework-mybatis/src/main/java/com/minimalism/mybatis/abs/service/IService.java
@@ -1,14 +1,10 @@
 package com.minimalism.mybatis.abs.service;
 
 
-import java.io.Serializable;
-import java.util.Map;
-
 /**
  * @Author yan
  * @Date 2025/3/10 17:25:21
  * @Description
  */
-public interface MpIService<T>  {
-
+public interface IService<T>  {
 }
